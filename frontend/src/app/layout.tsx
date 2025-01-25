5f53c3aee1ed4cf8ee38503c3a8cec242297f1ec
Guilherme Canesin <guilhermezagoc@gmail.com>
2025-01-25 18:56:07 -0300

    Importing Toster Item

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,14 +12,21 @@ export const metadata: Metadata = {
   description: "Lorem Ipsum Google Find Assist",
 };
 
+const toasterStyle = {
+  backgroundColor: "#f1f1f1",
+  color: "#131313",
+  borderColor: "rgba(255,255,255,0)",
+};
+
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
+        <Toaster position="top-center" toastOptions={{ style: toasterStyle }} />
         {children}
       </body>
     </html>
