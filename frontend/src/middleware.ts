c734dddc5a386ca14efa1c1051804348fd50071e
Guilherme Canesin <guilhermezagoc@gmail.com>
2025-01-25 18:54:28 -0300

    Adding token Middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,12 +4,16 @@ import { api } from './services/api';
 
 export async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
+    const token: string = await getCookieServer() as string;
+    const tokenIsValid: boolean = await validateToken(token);
 
     if (pathname.startsWith("/_next") || pathname === '/') {
+        if (tokenIsValid && (pathname === '/' || pathname === '/register')) {
+            return NextResponse.redirect(new URL("/main", req.url));
+        }
         return NextResponse.next();
     } else {
-        const token: string = await getCookieServer() as string;
-        if (await validateToken(token)) {
+        if (tokenIsValid) {
             return NextResponse.next();
         }
         return NextResponse.redirect(new URL("/", req.url));
