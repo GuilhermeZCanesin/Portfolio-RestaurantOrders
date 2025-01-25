62a8ea4624bb77f6cc7641da7b0adcacb5abe828
Guilherme Canesin <guilhermezagoc@gmail.com>
2025-01-25 18:55:03 -0300

    Updating Products Service

--- backend/src/features/products/product.service.ts
+++ backend/src/features/products/product.service.ts
@@ -2,72 +2,78 @@ import prismaClient from "../../prisma";
 import { ProductRequestInterface } from "./product.interface";
 
 export const getProductsByCategory = async (categoryId: string) => {
-    const productsFound = await prismaClient.product.findMany({
-        where: {
-            category_id: categoryId,
-        },
-        select: {
-            id: true,
-            name: true,
-        }
-    });
-    return productsFound;
+  const productsFound = await prismaClient.product.findMany({
+    where: {
+      category_id: categoryId,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+  return productsFound;
 };
 
 export const getProducts = async () => {
-    const productsFound = await prismaClient.product.findMany({
-        select: {
-            id: true,
-            name: true,
-        }
-    });
-    return productsFound;
+  const productsFound = await prismaClient.product.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+  return productsFound;
 };
 
 export const createProduct = async (product: ProductRequestInterface) => {
-    if (!product.name || !product.description || !product.price || !product.banner || !product.category_id) {
-        throw new Error('Missing data');
-    }
-    await productExists(product.name, false);
-    const productCreated = await prismaClient.product.create({
-        data: {
-            name: product.name,
-            description: product.description,
-            price: Number(product.price),
-            banner: product.banner,
-            category_id: product.category_id,
-        },
-        select: {
-            id: true,
-            name: true,
-        }
-    })
+  if (
+    !product.name ||
+    !product.description ||
+    !product.price ||
+    !product.banner ||
+    !product.category_id
+  ) {
+    console.log(product);
+    throw new Error("Missing data");
+  }
+  await productExists(product.name, false);
+  const productCreated = await prismaClient.product.create({
+    data: {
+      name: product.name,
+      description: product.description,
+      price: Number(product.price),
+      banner: product.banner,
+      category_id: product.category_id,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
 
-    return productCreated;
+  return productCreated;
 };
 
 export const deleteProduct = async (productId: string) => {
-    await productExists(productId, true);
-    const productDeleted = await prismaClient.product.delete({
-        where: {
-            id: productId
-        }
-    })
+  await productExists(productId, true);
+  const productDeleted = await prismaClient.product.delete({
+    where: {
+      id: productId,
+    },
+  });
 
-    return productDeleted;
+  return productDeleted;
 };
 
 const productExists = async (search: string, doesNot: boolean) => {
-    const productAlreadyExists = await prismaClient.product.findFirst({
-        where: {
-            OR: [{ name: search }, { id: search }],
-        }
-    })
+  const productAlreadyExists = await prismaClient.product.findFirst({
+    where: {
+      OR: [{ name: search }, { id: search }],
+    },
+  });
 
-    if (productAlreadyExists && !doesNot) {
-        throw new Error('Product already exists');
-    } else if (!productAlreadyExists && doesNot) {
-        throw new Error('Product does not exists')
-    }
-
-}
+  if (productAlreadyExists && !doesNot) {
+    throw new Error("Product already exists");
+  } else if (!productAlreadyExists && doesNot) {
+    throw new Error("Product does not exists");
+  }
+};
