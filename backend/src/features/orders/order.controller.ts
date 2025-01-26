974c62626f9c37d273bf594f485fd2217db72946
Guilherme Canesin <guilhermezagoc@gmail.com>
2025-01-25 23:25:54 -0300

    Updating Orders Service

--- backend/src/features/orders/order.controller.ts
+++ backend/src/features/orders/order.controller.ts
@@ -1,58 +1,86 @@
-import { Request, Response } from 'express';
-import * as orderService from './order.service';
-import { OrderRequestInterface } from './order.interface';
-
+import { Request, Response } from "express";
+import * as orderService from "./order.service";
+import { OrderRequestInterface } from "./order.interface";
 
 export const getOrders = async (req: Request, res: Response): Promise<void> => {
-    const orderId: string = req.query.id as string;
-    const tableNbr: string = req.query.table as string;
+  const orderId: string = req.query.id as string;
+  const tableNbr: string = req.query.table as string;
+  const status: string = req.query.status as string;
 
-    try {
-        if (orderId) {
-            const order = await orderService.getOrderById(orderId);
-            res.json(order);
-            return;
-        }
-        if (tableNbr) {
-            const order = await orderService.getOrdersFromTable(Number(tableNbr));
-            res.json(order);
-            return;
-        }
-        const order = await orderService.getOrders();
-        res.json(order);
-    } catch (error) {
-        res.status(500).json({ message: 'Could not get Orders!', error: (error as Error).message });
+  try {
+    if (orderId) {
+      const order = await orderService.getOrderById(orderId);
+      res.json(order);
+      return;
     }
+    if (tableNbr) {
+      const order = await orderService.getOrdersFromTable(Number(tableNbr));
+      res.json(order);
+      return;
+    }
+    const order = await orderService.getOrders(status ? true : false);
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({
+      message: "Could not get Orders!",
+      error: (error as Error).message,
+    });
+  }
 };
 
-export const createOrder = async (req: Request, res: Response): Promise<void> => {
-    const order: OrderRequestInterface = req.body;
+export const createOrder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const order: OrderRequestInterface = req.body;
 
-    try {
-        const orderCreated = await orderService.createOrder(order);
-        res.status(201).json({ message: 'Order created successfully!', orderCreated });
-    } catch (error) {
-        res.status(400).json({ message: 'Order creation failed!', error: (error as Error).message });
-    }
+  try {
+    const orderCreated = await orderService.createOrder(order);
+    res
+      .status(201)
+      .json({ message: "Order created successfully!", orderCreated });
+  } catch (error) {
+    res.status(400).json({
+      message: "Order creation failed!",
+      error: (error as Error).message,
+    });
+  }
 };
 
-export const updateOrder = async (req: Request, res: Response): Promise<void> => {
-    const order: OrderRequestInterface = req.body;
-    try {
-        const orderUpdated = await orderService.updateOrder(order);
-        res.status(201).json({ message: 'Order updated successfully!', orderUpdated });
-    } catch (error) {
-        res.status(400).json({ message: 'Order update failed!', error: (error as Error).message });
-    }
+export const updateOrder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const order: OrderRequestInterface = req.body;
+  try {
+    const orderUpdated = await orderService.updateOrder(order);
+    res
+      .status(201)
+      .json({ message: "Order updated successfully!", orderUpdated });
+  } catch (error) {
+    res.status(400).json({
+      message: "Order update failed!",
+      error: (error as Error).message,
+    });
+  }
 };
 
-export const deleteOrder = async (req: Request, res: Response): Promise<void> => {
-    const orderDeletedId: string = req.query.id as string;
+export const deleteOrder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const orderDeletedId: string = req.query.id as string;
 
-    try {
-        const orderDeleted = await orderService.deleteOrder(orderDeletedId);
-        res.status(201).json({ message: 'Order deleted successfully!', order: { name: orderDeleted.name, table: orderDeleted.table }, });
-    } catch (error) {
-        res.status(400).json({ message: 'Order deletion failed!', error: (error as Error).message });
-    }
+  try {
+    const orderDeleted = await orderService.deleteOrder(orderDeletedId);
+    res.status(201).json({
+      message: "Order deleted successfully!",
+      order: { name: orderDeleted.name, table: orderDeleted.table },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Order deletion failed!",
+      error: (error as Error).message,
+    });
+  }
 };
--- backend/src/features/orders/order.service.ts
+++ backend/src/features/orders/order.service.ts
@@ -1,123 +1,127 @@
 import prismaClient from "../../prisma";
 import { OrderRequestInterface } from "./order.interface";
 
-
 export const getOrderById = async (orderId: string) => {
-    const orderFound = await prismaClient.order.findFirst({
-        where: {
-            id: orderId,
-        },
+  const orderFound = await prismaClient.order.findFirst({
+    where: {
+      id: orderId,
+    },
+    include: {
+      items: {
         include: {
-            items: {
-                include: {
-                    product: true
-                }
-            }
-        }
-    })
-    return orderFound;
+          product: true,
+        },
+      },
+    },
+  });
+  return orderFound;
 };
 
 export const getOrdersFromTable = async (tableNbr: number) => {
-    const ordersFound = await prismaClient.order.findMany({
-        where: {
-            table: tableNbr,
-        },
+  const ordersFound = await prismaClient.order.findMany({
+    where: {
+      table: tableNbr,
+    },
+    include: {
+      items: {
         include: {
-            items: {
-                include: {
-                    product: true
-                }
-            }
-        }
-    })
-    return ordersFound;
+          product: true,
+        },
+      },
+    },
+  });
+  return ordersFound;
 };
 
-export const getOrders = async () => {
-    const ordersFound = await prismaClient.order.findMany();
-    return ordersFound;
+export const getOrders = async (status: boolean) => {
+  const ordersFound = await prismaClient.order.findMany({
+    where: {
+      status: status,
+    },
+  });
+  return ordersFound;
 };
 
 export const createOrder = async (order: OrderRequestInterface) => {
-    if (!order.table) {
-        throw new Error('Missing data');
-    }
-    const orderCreated = await prismaClient.order.create({
-        data: {
-            table: order.table,
-            name: order.name,
-        },
-        select: {
-            id: true,
-            name: true,
-        }
-    })
+  if (!order.table) {
+    throw new Error("Missing data");
+  }
+  const orderCreated = await prismaClient.order.create({
+    data: {
+      table: order.table,
+      name: order.name,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
 
-    return orderCreated;
+  return orderCreated;
 };
 export const updateOrder = async (order: OrderRequestInterface) => {
-    if (!order.id) {
-        throw new Error('Missing data');
-    }
-    await orderDoesNotExist(order.id);
-    const orderUpdated = await prismaClient.order.update({
-        data: {
-            table: order.table,
-            name: order.name,
-            draft: order.draft,
-        },
-        where: {
-            id: order.id
-        }
-    })
+  if (!order.id) {
+    throw new Error("Missing data");
+  }
+  await orderDoesNotExist(order.id);
+  const orderUpdated = await prismaClient.order.update({
+    data: {
+      table: order.table,
+      name: order.name,
+      draft: order.draft,
+    },
+    where: {
+      id: order.id,
+    },
+  });
 
-    return orderUpdated;
+  return orderUpdated;
 };
 
 export const emitOrder = async (orderId: string) => {
-    await orderDoesNotExist(orderId);
-    const orderDeleted = await prismaClient.order.update({
-        where: {
-            id: orderId,
-        }, data: {
-            draft: false,
-        }
-    })
+  await orderDoesNotExist(orderId);
+  const orderDeleted = await prismaClient.order.update({
+    where: {
+      id: orderId,
+    },
+    data: {
+      draft: false,
+    },
+  });
 
-    return orderDeleted;
+  return orderDeleted;
 };
 
 export const deleteOrder = async (orderId: string) => {
-    await orderDoesNotExist(orderId);
-    const orderDeleted = await prismaClient.order.delete({
-        where: {
-            id: orderId
-        }
-    })
+  await orderDoesNotExist(orderId);
+  const orderDeleted = await prismaClient.order.delete({
+    where: {
+      id: orderId,
+    },
+  });
 
-    return orderDeleted;
+  return orderDeleted;
 };
 
 const orderDoesNotExist = async (orderId: string) => {
-    const foundOrder = await prismaClient.order.findFirst({
-        where: {
-            id: orderId,
-        },
-    })
-    if (!foundOrder) {
-        throw new Error('Order does not exist');
-    }
-}
+  const foundOrder = await prismaClient.order.findFirst({
+    where: {
+      id: orderId,
+    },
+  });
+  if (!foundOrder) {
+    throw new Error("Order does not exist");
+  }
+};
 
 const orderAlreadyExists = async (order: OrderRequestInterface) => {
-    const orderAlreadyExists = await prismaClient.order.findFirst({
-        where: {
-            table: order.table,
-            name: order.name,
-        }
-    })
-    if (orderAlreadyExists) {
-        throw new Error('Order already exists');
-    }
-}
+  const orderAlreadyExists = await prismaClient.order.findFirst({
+    where: {
+      table: order.table,
+      name: order.name,
+    },
+  });
+  if (orderAlreadyExists) {
+    throw new Error("Order already exists");
+  }
+};
